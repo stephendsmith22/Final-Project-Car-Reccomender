5bb93a742d1b8d936aee278a8b6fe6500529cc9d
Stephen Smith <stephendsmith22@gmail.com>
2023-04-27 18:23:35 -0400

    Completed Quick Sort sorting algorithm for garage

--- Project 3_Car Finder/Garage.cpp
+++ Project 3_Car Finder/Garage.cpp
@@ -9,7 +9,7 @@ using namespace std;
 Garage::Garage(ifstream& file, string make, string model) {
 	//get rid of the first line because it's just the column titles
 	getline(file, tempString);
-	while (file.is_open()) {
+	while (file.is_open() && garage.size() < 10) {
 		//read in id, if our tempString is blank, then we have reached the end of the file
 		getline(file, tempString, ',');
 		if (tempString.size() == 0)
@@ -51,7 +51,6 @@ void Garage::swap(Cars& a, Cars& b){
 
 int Garage::partition(vector<Cars> map_garage, int start, int end, string sortingChoice){
 	if (sortingChoice == "price") {
-
 		int pivot = garage[start].getPrice();
 
 		int count = 0;
@@ -75,17 +74,80 @@ int Garage::partition(vector<Cars> map_garage, int start, int end, string sortin
 			while (garage[j].getPrice() > pivot)
 				j--;
 
-			if (i < pivotIndex && j > pivotIndex)
-				swap(garage[i++], garage[j--]);
+			if (i < pivotIndex && j > pivotIndex) {
+				swap(garage[i], garage[j]);
+				i++;
+				j--;
+			}
 		}
 
 		return pivotIndex;
 	}
 	else if (sortingChoice == "mileage") {
+		int pivot = garage[start].getMileage();
 
+		int count = 0;
+		for (int i = start + 1; i <= end; i++) {
+			if (garage[i].getMileage() <= pivot)
+				count++;
+		}
+
+		// Giving pivot element its correct position
+		int pivotIndex = start + count;
+		swap(garage[pivotIndex], garage[start]);
+
+		// Sorting left and right parts of the pivot element
+		int i = start, j = end;
+
+		while (i < pivotIndex && j > pivotIndex) {
+
+			while (garage[i].getMileage() <= pivot)
+				i++;
+
+			while (garage[j].getMileage() > pivot)
+				j--;
+
+			if (i < pivotIndex && j > pivotIndex) {
+				swap(garage[i], garage[j]);
+				i++;
+				j--;
+			}
+		}
+
+		return pivotIndex;
 	}
 	else if (sortingChoice == "year") {
+		int pivot = garage[start].getYear();
 
+		int count = 0;
+		for (int i = start + 1; i <= end; i++) {
+			if (garage[i].getYear() <= pivot)
+				count++;
+		}
+
+		// Giving pivot element its correct position
+		int pivotIndex = start + count;
+		swap(garage[pivotIndex], garage[start]);
+
+		// Sorting left and right parts of the pivot element
+		int i = start, j = end;
+
+		while (i < pivotIndex && j > pivotIndex) {
+
+			while (garage[i].getYear() <= pivot)
+				i++;
+
+			while (garage[j].getYear() > pivot)
+				j--;
+
+			if (i < pivotIndex && j > pivotIndex) {
+				swap(garage[i], garage[j]);
+				i++;
+				j--;
+			}
+		}
+
+		return pivotIndex;
 	}
 }
 
